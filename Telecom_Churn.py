93bede74c50769cda05d85b0760cdbe1c910b862
Prabin <2308806@live.stmarys.ac.uk>
2024-10-25 23:05:42 +0100

    updated code to add total no. of rows and columns presented in the dataset

--- Telecom_Churn.py
+++ Telecom_Churn.py
@@ -1,23 +1,26 @@
-import pandas as pd
 
 import pandas as pd
 
-class ChurnAnalysis:
+class Churn_Analysis:
 
     def __init__(self, data_path):
-        """Initialize the ChurnAnalysis class with the dataset path"""
-        # Load the dataset
+        """Initializing the ChurnAnalysis class with the dataset path"""
+        # Loading the dataset
         self.data = pd.read_csv(data_path, header=0, sep=',')
-
-        # Display success message
-        print("Dataset loaded successfully!")
         print('Dataset:')
         print(self.data.head())
 
-        #Display the number of rows in the dataset
-        print('\nNumber of rows:', self.data.shape[0], '\n')
+        # Display the number of rows in the dataset
+        print('\nTotal Number of rows:', self.data.shape[0])
+
+        # Display the number of columns in the dataset
+        print('Total Number of columns:', self.data.shape[1], '\n')
+
+        # Displaying success message
+        print("Dataset loaded successfully!")
+
 
 if __name__ == "__main__":
-    # Create instance of ChurnAnalysis
-    analysis = ChurnAnalysis("Telco-Customer-Churn.csv")
+    # Creating instance of Churn_Analysis
+    analysis = Churn_Analysis("Telco-Customer-Churn.csv")
 
